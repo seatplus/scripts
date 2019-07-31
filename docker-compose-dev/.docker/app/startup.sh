472067ac5e0587ce23f664f60e7154d3f79bf297
Felix Huber <felix.huber@husqvarnagroup.com>
2019-07-31 17:43:42 +0200

    update dockerfile etc for development

--- docker-compose-dev/.docker/app/startup.sh
+++ docker-compose-dev/.docker/app/startup.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 set -e
 
+# Ensure the latest sources from this container lives in the volume.
+# Working dir is /var/www from the container.
+#tar cf - --one-file-system --overwrite -C /seatplus . | tar xf -
+
+chown -R www-data:www-data storage
+
+# Ensure we have .env, if we dont, try and fix it automatically
+if [ ! -f .env ]; then \
+    cp .env.example .env && \
+
+    # Fix up MariaDB and Redis connection info
+    sed -i -- 's/DB_USERNAME=seatplus/DB_USERNAME='$MYSQL_USER'/g' .env
+    sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD='$MYSQL_PASSWORD'/g' .env
+    sed -i -- 's/DB_DATABASE=seatplus/DB_DATABASE='$MYSQL_DATABASE'/g' .env
+    sed -i -- 's/DB_HOST=127.0.0.1/DB_HOST=mariadb/g' .env
+    sed -i -- 's/APP_DEBUG=false/APP_DEBUG=true/g' .env
+    sed -i -- 's/REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g' .env
+
+    php artisan key:generate
+fi
+
 # Wait for the database
 while ! mysqladmin ping -hmariadb -u$MYSQL_USER -p$MYSQL_PASSWORD --silent; do
 
@@ -8,42 +29,10 @@ while ! mysqladmin ping -hmariadb -u$MYSQL_USER -p$MYSQL_PASSWORD --silent; do
     sleep 3
 done
 
-# Ensure we have vendor/ ready
-if [ ! -f /var/www/vendor/autoload.php ]; then
-
-    echo "seatplus container might not be ready yet... sleeping..."
-    chown -R www-data:www-data storage
-
-    # Ensure we have .env, if we dont, try and fix it automatically
-    if [ ! -f /var/www/seat/.env ]; then
-
-        cp /var/www/.env.example /var/www/.env
-
-        # Fix up MariaDB and Redis connection info
-        sed -i -- 's/DB_USERNAME=seatplus/DB_USERNAME='$MYSQL_USER'/g' .env
-        sed -i -- 's/DB_PASSWORD=secret/DB_PASSWORD='$MYSQL_PASSWORD'/g' .env
-        sed -i -- 's/DB_DATABASE=seatplus/DB_DATABASE='$MYSQL_DATABASE'/g' .env
-        sed -i -- 's/DB_HOST=127.0.0.1/DB_HOST=mariadb/g' .env
-        sed -i -- 's/APP_DEBUG=false/APP_DEBUG=true/g' .env
-        sed -i -- 's/REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g' .env
-    fi
-
-    composer install
-    php artisan key:generate
-
-    # Publish assets and migrate the database
-    php artisan vendor:publish --force --all
-    php artisan migrate
-
-fi
-
-# Ensure we have vendor/ ready
-while [ ! -f /var/www/vendor/autoload.php ]
-do
-    echo "seatplus container might not be ready yet... sleeping..."
-    sleep 30
-
-done
+npm install && npm run development
+composer update
+php artisan vendor:publish --force --all
+php artisan migrate
 
 php-fpm -F
 
--- docker-compose-dev/prepare-source.sh
+++ docker-compose-dev/prepare-source.sh
@@ -7,7 +7,7 @@ openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -config ./opens
 echo "Pulling sources..."
 
 git clone https://github.com/seatplus/core seatplus && cd seatplus
-cp ../composer.json composer.json
+#cp ../composer.json composer.json
 
 mkdir -p packages/seatplus && cd packages/seatplus
 git clone https://github.com/seatplus/web
